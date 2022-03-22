66a18c61ecf6fc9165f5c5780be71142c447a41d
david-xi-long <david.xi.long@gmail.com>
2022-03-22 11:34:41 -0400

    Fixed typos

--- src/main/java/com/tvshowdatabase/backend/repository/ActorRepository.java
+++ src/main/java/com/tvshowdatabase/backend/repository/ActorRepository.java
@@ -1,10 +1,10 @@
 package com.tvshowdatabase.backend.repository;
 
-import com.tvshowdatabase.backend.models.TVShow;
+import com.tvshowdatabase.backend.models.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface ActorRepository extends JpaRepository<TVShow, Integer> {
+public interface ActorRepository extends JpaRepository<Actor, Integer> {
 }
--- src/main/java/com/tvshowdatabase/backend/repository/GenreRepository.java
+++ src/main/java/com/tvshowdatabase/backend/repository/GenreRepository.java
@@ -1,10 +1,10 @@
 package com.tvshowdatabase.backend.repository;
 
-import com.tvshowdatabase.backend.models.TVShow;
+import com.tvshowdatabase.backend.models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface GenreRepository extends JpaRepository<TVShow, Integer> {
+public interface GenreRepository extends JpaRepository<Genre, Integer> {
 }

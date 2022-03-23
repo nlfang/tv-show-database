42206904ea94bd789db34bde384e53aa9846a311
david-xi-long <david.xi.long@gmail.com>
2022-03-22 23:36:12 -0400

    Added some pathing and form for adding shows

--- src/main/java/com/tvshowdatabase/backend/controller/TVShowController.java
+++ src/main/java/com/tvshowdatabase/backend/controller/TVShowController.java
@@ -2,9 +2,9 @@ package com.tvshowdatabase.backend.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.tvshowdatabase.backend.models.TVShow;
 import com.tvshowdatabase.backend.repository.TVShowRepository;
@@ -15,6 +15,13 @@ public class TVShowController {
     private TVShowRepository tvShowRepository;
     @GetMapping("/tvshows")
     public List<TVShow> getAllTVShows() {
+        System.out.println("Reached get all TV Shows");
         return tvShowRepository.findAll();
+    }
+
+    @PostMapping("/tvshows")
+    public ResponseEntity<TVShow> addShow(@RequestBody TVShow tvShow) {
+        System.out.println(tvShow.getShowID() + "Show ID");
+        return new ResponseEntity<TVShow>(tvShowRepository.save(tvShow), HttpStatus.OK);
     }
 }
--- tvdb-frontend/src/App.js
+++ tvdb-frontend/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import TVShowComponent from './components/TVShowComponent';
 import UserComponent from './components/UserComponent';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import AddTVShowComponent from './components/AddTVShowComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <TVShowComponent/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}>
+          </Route>
+          <Route path="/tvshows" element={<TVShowComponent/>}>
+          </Route>
+          <Route path="/users" element={<UserComponent/>}>
+          </Route>
+          <Route path="/addtvshow" element={<AddTVShowComponent/>}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
